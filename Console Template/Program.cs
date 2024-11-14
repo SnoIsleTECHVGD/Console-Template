c83068fc887622008a3ee23b4a6f496127c97a60
Derek <dereksturm263@gmail.com>
2024-11-14 13:42:46 -0800

    Initial push

---
+++ Console Template/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Console_Template
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
